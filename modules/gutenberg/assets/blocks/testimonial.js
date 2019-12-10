f7c551d07cce61923c82347afa257ffb80cef3cf
RajuJeelaga <rajujeelaga@gmail.com>
2019-12-10 19:35:15 +0530

    testimonial repeater field is added
    testimonial repeater field is added

--- modules/gutenberg/assets/blocks/testimonial.js
+++ modules/gutenberg/assets/blocks/testimonial.js
@@ -27,52 +27,64 @@
         supports: {
             multiple: true
         },
-        
         attributes: {
-            btn_clicked:{
-              type:Boolean,
-              default:false
-            },
-            alignment: {
-              type: 'string',
-              default: 'center',
-            },
-            testi_content: {
-              type: 'string',
-              default :'You can Decide whether to create your site using UI Kit blocks or samples. The blocks can merge together in various combinations.',
-            },
-            mediaID: {
-              type: 'number'
-            },
-            mediaURL: {
-              type: 'string',   
-              default: 'http://localhost/ampdev/wp-content/uploads/2019/12/user-df-img.png',
-            },
-            testi_authr_nm: {
-              type: 'string',
-              default :'Raju Jeelaga',
-            },
-            testi_social_fld_nm: {
-              type: 'string',
-              default :'GOOGLE',
-            },
-            testi_content_color:{
-              type:'string',
-              default: '#54479a',
-            },
-            testi_authr_nm_color:{
-              type:'string',
-              default: '#54479a',
-            },
-            testi_social_fld_nm_color:{
-              type:'string',
-              default: '#b0afb4',
-            },
+          id: {
+            source: "attribute",
+            selector: ".carousel.slide",
+            attribute: "id"
+          },
+          btn_clicked:{
+            type:Boolean,
+            default:false
+          },
+          alignment: {
+            type: 'string',
+            default: 'center',
+          },
+          items: {
+            source: "query",
+            default: [],
+            selector: "blockquote.testimonial",
+            query: {
+              index: {            
+                type: 'number',                  
+                attribute: 'data-index',                  
+              },
+              testi_content: {
+                type: 'string',
+              },
+              mediaID: {
+                type: 'number'
+              },
+              mediaURL: {
+                type: 'string',
+              },
+              testi_authr_nm: {
+                type: 'string',
+              },
+              testi_social_fld_nm: {
+                type: 'string',
+              },
+              isSelected: {            
+                type: 'boolean',
+                default:false      
+              },
+              testi_content_color:{
+                type:'string',
+              },
+              testi_authr_nm_color:{
+                type:'string',
+              },
+              testi_social_fld_nm_color:{
+                type:'string',
+              },
+            } // query ends
+          } // Testimonial selector ends
+        }, // attributes ends
 
-        },               
         edit: function(props) {
 
-        const colors = [
+          const colors = [
             { name: 'pink', color: '#f78da7' },
             { name: 'red', color: '#cf2e2e' },
             { name: 'orange', color: '#ff6900' },
@@ -89,172 +101,194 @@
 
         ];
 
-        var attributes = props.attributes;    
-        var alignment  = props.attributes.alignment;
 
+        function _cloneArray(arr) { 
+          if (Array.isArray(arr)) { 
+              for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { 
+                arr2[i] = arr[i]; 
+              } 
+              return arr2; 
+          } else { 
+              return Array.from(arr); 
+          } 
+      }
 
-        var media_upload = el(MediaUpload, {
-          onSelect: function(media){  
-            props.setAttributes( { mediaURL: media.url} );
-            //console.log(media);
-          },
+        
 
-         allowedTypes:[ "image" ],
-         render:function(obj){
-             return el( 'img', {                  
-                  className: 'ab-tst-img',            
-                  onClick: obj.open,
-                  src:attributes.mediaURL
-                }            
-          )
-         }
-      });
+      
+     var itemlist =  props.attributes.items.map(function(key){
 
-        var testimonial_cnt = el(RichText,{
-          tagName: 'div',
-          className: 'ab-tsti-cnt',
-          style: { color: attributes.testi_content_color,
-           },                             
-          autoFocus: true,                               
-          value: attributes.testi_content,
-          onChange : function( event ){
-            props.setAttributes( { testi_content: event} );
-          }
-        });
+        return el('li',{
+          
+        },
+            el(RichText,{
+              tagName: 'div',
+              className: 'ab-tsti-cnt',
+              placeholder: __('Enter the Testimonial', 'amp-blocks'),
+              style: { color: key.testi_content_color,
+              },                             
+              autoFocus: true,                               
+              value: key.testi_content,
+              onChange : function( event ){
+                props.setAttributes( { testi_content: event} );
+               }
+            }),
 
-        var testimonial_nme = el(RichText,{
-          tagName: 'div',
-          className: 'ab-tsti-nm',
-          style: { color: attributes.testi_authr_nm_color,
-           },                             
-          autoFocus: true,                               
-          value: attributes.testi_authr_nm,
-          onChange : function( event ){
-            props.setAttributes( { testi_authr_nm: event} );
-          }
-        });
-
-        var testimonial_spc = el(RichText,{
-          tagName: 'div',
-          className: 'ab-tsti-spf',
-          style: { color: attributes.testi_social_fld_nm_color,
-           },                             
-          autoFocus: true,                               
-          value: attributes.testi_social_fld_nm,
-          onChange : function( event ){
-            props.setAttributes( { testi_social_fld_nm: event} );
-          }
-        });
-
-
-        var testi_wrap = el('div',{className: 'ab-tsti-w', 
-          style: { textAlign: attributes.alignment} 
-            },testimonial_cnt, media_upload, testimonial_nme, testimonial_spc);
-
-
-            return [el(InspectorControls,
-              {
-               className:'ampblocks-btn-fields',
-               key: 'inspector'   
+            el(MediaUpload, {
+              onSelect: function(media){  
+                props.setAttributes( { mediaURL: media.url} );
               },
-                el(PanelBody,
-                  {className:'ampblocks-btn-layout-stng',
-                  initialOpen: true,
-                  title:'Layout Settings'   
-                  },
-                   
-                    // Display alignment toolbar within block controls.
-                    el('span',{className:"cntrl-lbl"},__('Alignment', 'amp-blocks')),
-                    el(AlignmentToolbar, {
-                      value: alignment,
-                      onChange: function(event){
-                        props.setAttributes({ alignment: event })
-                      }
-                    }),
-
-                   
-                  ), // Layout Settings Ends
-
-              el(PanelBody,{
-                className:'ampblocks-tst-color-stng',
-                initialOpen: false,
-                title:'Color Settings',
-              },
-                
-              el('div',{className:"sub-hd-clr", },
-                  el('span',{},__('Content Color', 'amp-blocks')),
-                  el('div',{},el(ColorPalette,{
-                    className:"ampb-tst-cnt",
-                    colors: colors,
-                    onChange: function(event){
-                      props.setAttributes( { testi_content_color:event } );
-                    } 
-                  })),
-              ),
-              el('div',{className:"sub-hd-clr", },
-                  el('span',{},__('Testimonial Name Color', 'amp-blocks')),
-                  el('div',{},el(ColorPalette,{
-                    className:"ampb-tst-nm",
-                    colors: colors,
-                    onChange: function(event){
-                      props.setAttributes( { testi_authr_nm_color:event } );
-                    } 
-                  })),
-              ),
-              el('div',{className:"sub-hd-clr", },
-                  el('span',{},__('Social Profile Text Color', 'amp-blocks')),
-                  el('div',{},el(ColorPalette,{
-                    className:"ampb-tst-spc",
-                    colors: colors,
-                    onChange: function(event){
-                      props.setAttributes( { testi_social_fld_nm_color:event } );
-                    } 
-                  })),
+    
+             allowedTypes:[ "image" ],
+             render:function(obj){
+                 return el( 'img', {                  
+                      className: 'ab-tst-img',            
+                      onClick: obj.open,
+                      src:key.mediaURL
+                    }            
               )
-                  
+             }
+          }),
 
-              ), // Color Settings Ends
+            el(RichText,{
+              tagName: 'div',
+              className: 'ab-tsti-nm',
+              placeholder: __('Name', 'amp-blocks'),
+              style: { color: key.testi_authr_nm_color,
+               },                             
+              autoFocus: true,                               
+              value: key.testi_authr_nm,
+              onChange : function( event ){
+                props.setAttributes( { testi_authr_nm: event} );
+              }
+            }),
 
-              ),
-              testi_wrap
-            ];
+            el(RichText,{
+              tagName: 'div',
+              className: 'ab-tsti-spf',
+              placeholder: __('Google', 'amp-blocks'),
+              style: { color: key.testi_social_fld_nm_color,
+               },                             
+              autoFocus: true,                               
+              value: key.testi_social_fld_nm,
+              onChange : function( event ){
+                props.setAttributes( { testi_social_fld_nm: event} );
+              }
+            }),
 
-        },// edit ends here
+        ) // </li> ends here
+
+      }); // Items ends here
+
+       var repeater =  el( IconButton, {
+          icon: "insert",
+          className: 'ab-repeater',            
+          onClick: function() {              
+            return props.setAttributes({
+              items: [].concat(_cloneArray(props.attributes.items), [{
+                index: props.attributes.items.length,
+                alignment : 'center',                  
+                testi_content: "You can Decide whether to create your site using UI Kit blocks or samples. The blocks can merge together in various combinations.",
+                mediaURL:'http://localhost/ampdev/wp-content/uploads/2019/12/user-df-img.png',
+                testi_authr_nm:'Raju Jeelaga',
+                testi_social_fld_nm: 'GOOGLE',
+                testi_content_color:'#54479a',
+                testi_authr_nm_color:'#54479a',
+                testi_social_fld_nm_color:'#b0afb4',
+              }])
+            });                            
+          }
+        },
+        __('Add a Testimonial', 'amp-blocks')
+      );
+
+       var parentdiv = el('div',{
+         className: "ab-tsti-w"
+       },
+       itemlist,
+        repeater  
+       ); 
+
+      // Inspector Controls Starts
+       return [el(InspectorControls,
+        {
+         className:'ampblocks-btn-fields',
+         key: 'inspector'   
+        },
+          el(PanelBody,
+            {className:'ampblocks-tsti-layout-stng',
+            initialOpen: true,
+            title:'Layout Settings'   
+            },
+             
+              //Display alignment toolbar within block controls.
+              // el('span',{className:"cntrl-lbl"},__('Alignment', 'amp-blocks')),
+              // el(AlignmentToolbar, {
+              //   value: alignment,
+              //   onChange: function(event){
+              //     props.setAttributes({ alignment: event })
+              //   }
+              // }),
+
+             
+            ), // Layout Settings Ends
+
+        el(PanelBody,{
+          className:'ampblocks-tst-color-stng',
+          initialOpen: false,
+          title:'Color Settings',
+        },
+          
+        el('div',{className:"sub-hd-clr", },
+            el('span',{},__('Content Color', 'amp-blocks')),
+            el('div',{},el(ColorPalette,{
+              className:"ampb-tst-cnt",
+              colors: colors,
+              onChange: function(event){
+                props.setAttributes( { testi_content_color:event
+                   } );
+              } 
+            })),
+        ),
+        el('div',{className:"sub-hd-clr", },
+            el('span',{},__('Testimonial Name Color', 'amp-blocks')),
+            el('div',{},el(ColorPalette,{
+              className:"ampb-tst-nm",
+              colors: colors,
+              onChange: function(event){
+                props.setAttributes( { testi_authr_nm_color:event } );
+              } 
+            })),
+        ),
+        el('div',{className:"sub-hd-clr", },
+            el('span',{},__('Social Profile Text Color', 'amp-blocks')),
+            el('div',{},el(ColorPalette,{
+              className:"ampb-tst-spc",
+              colors: colors,
+              onChange: function(event){
+                props.setAttributes( { testi_social_fld_nm_color:event } );
+              } 
+            })),
+        )
+            
+
+        ), // Color Settings Ends
+
+        ),
+
+       parentdiv
+     ];
+
+      }, // Edit Ends Here
 
         save: function( props ) {
 
-          var tsti_cnt = el( 'div', {
-            className: 'ab-tsti-cnt',
-             style: { color: props.attributes.testi_content_color,
-                    },
-          }, props.attributes.testi_content);
+          return null;
+        }
+        
 
-          var upload_image = el( 'img' , {
-            className : 'ab-tst-img',
-            src: props.attributes.mediaURL
-          });
-
-          var tsti_nm = el( 'div', {
-            className: 'ab-tsti-nm',
-             style: { color: props.attributes.testi_authr_nm_color,
-                    },
-          }, props.attributes.testi_authr_nm);
-
-          var tsti_spf = el( 'div', {
-            className: 'ab-tsti-spf',
-             style: { color: props.attributes.testi_social_fld_nm_color,
-                    },
-          }, props.attributes.testi_social_fld_nm);
-
-          
-          var tstiwrapper = el( 'div',{className: 'ab-tsti-w', 
-                                       style: { textAlign: props.attributes.alignment } 
-                              },tsti_cnt, upload_image, tsti_nm, tsti_spf);
-
-          return tstiwrapper;
-
-          }
-    } );
+        
+    } ); // RegisterBlockType Ends
 }(
     window.wp.blocks,
     window.wp.element,
