30c5489c5991f55a414733c8504f88b9954e97a0
RajuJeelaga <rajujeelaga@gmail.com>
2020-01-07 19:29:51 +0530

    video block is added properly
    video block is added properly

--- src/blocks/video/block.js
+++ src/blocks/video/block.js
@@ -4,22 +4,20 @@
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
  */
-
 import './style.css';
 import './editor.css';
-
+import edit from './edit';
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 const { MediaUpload } = wp.editor; //Import MediaUpload from wp.editor
 const { Button } = wp.components; //Import Button from wp.components
-
 const {
-	RichText,
-	URLInput,
-	InspectorControls,
-	BlockControls,
-	AlignmentToolbar,
-	InspectorAdvancedControls,
+    RichText,
+    URLInput,
+    InspectorControls,
+    BlockControls,
+    AlignmentToolbar,
+    InspectorAdvancedControls,
 } = wp.blockEditor;
 /**
  * Register: aa Gutenberg Block.
@@ -34,9 +32,8 @@ const {
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-
-registerBlockType( 'ampblocks/video', {
-    title: __( 'Video' ), // Block title.
+registerBlockType('ampblocks/video', {
+    title: __('Video'), // Block title.
     icon: 'format-video', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
     category: 'amp-blocks', // Block category
     keywords: [ //Keywords
@@ -44,82 +41,19 @@ registerBlockType( 'ampblocks/video', {
         __('videos')
     ],
     attributes: { //Attributes
-        images : { //Images array
-            type: 'array',
+        videolink_demo: {
+            type: Boolean,
+            default: false
+        },
+        videoSource: {
+            type: Boolean,
+            default: false
+        },
+        images: { //Images array
+            type: 'string',
         }
-},
-
-    /**
-     * The edit function describes the structure of your block in the context of the editor.
-     * This represents what the editor will render when the block is used.
-     *
-     * The "edit" property must be a valid function.
-     *
-     * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-     */
-    edit({ attributes, className, setAttributes }) {
-
-        //Destructuring the images array attribute
-        const {images = []} = attributes;
-
-
-        // This removes an image from the gallery
-        const removeImage = (removeImage) => {
-                    //filter the images
-                    const newImages = images.filter( (image) => {
-                        //If the current image is equal to removeImage the image will be returnd
-                        if(image.id != removeImage.id) {
-                            return image;
-                        }
-                    });
-
-                    //Saves the new state
-                    setAttributes({
-                        images:newImages,
-                    })
-        }
-
-
-        //Displays the images
-        const displayImages = (images) => {
-            return (
-                //Loops throug the images
-                images.map( (image) => {
-                    return (
-                    <div className="gallery-item-container">
-                            <iframe className='gallery-item' src={image.url} key={ images.id } />
-                            <div className='remove-item' onClick={() => removeImage(image)}><span class="dashicons dashicons-trash"></span></div>
-                            <div className='caption-text'>{image.caption[0]}</div>
-                    </div>
-                    )
-                })
-
-            )
-        }
-
-        //JSX to return
-        return (
-            <div>
-                <div className="gallery-grid">
-                    {displayImages(images)}
-                </div>
-                <br/>
-                <MediaUpload
-                        onSelect={(media) => {setAttributes({images: [...images, ...media]});}}
-                        type="image"
-                        multiple={true}
-                        value={images}
-                        render={({open}) => (
-                            <Button className="select-images-button is-button is-default is-large" onClick={open}>
-                                Upload Video
-                            </Button>
-                        )}
-                    />
-            </div>
-
-        );
     },
-
+    edit,
     /**
      * The save function defines the way in which the different attributes should be combined
      * into the final markup, which is then serialized by Gutenberg into post_content.
@@ -128,34 +62,44 @@ registerBlockType( 'ampblocks/video', {
      *
      * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
      */
-    save({attributes}) {
-        //Destructuring the images array attribute
-        const { images = [] } = attributes;
-
-        // Displays the images
-        const displayImages = (images) => {
-            return (
-                images.map( (image,index) => {
-                    return (
-                            <iframe
-                                className='gallery-item'
-                                key={images.id}
-                                src={image.url}
-                                data-slide-no={index}
-                                data-caption={image.caption[0]}
-                                alt={image.alt}
-                                />
-                    )
-                })
-            )
+    save({ attributes }) {
+        console.log("i am bigboss");
+        console.log(attributes);
+        const { images, videoSource } = attributes;
+        const getId = (url) => {
+            let name = 'v';
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return '//www.youtube.com/embed/' + decodeURIComponent(results[2].replace(/\+/g, " "));
         }
-
-        //JSX to return
+        let displayImages = (images) => {
+            if (videoSource) {
+                return (
+                    <div className="gallery-item-container">
+                        <iframe width="1000px" height="500px" className='gallery-item' src={getId(images)} />
+                    </div>
+                )
+            } else {
+                //Loops throug the images
+                if (typeof images !== 'undefined') {
+                    return (
+                        <div className="gallery-item-container">
+                            <iframe width="1000px" height="500px" className='gallery-item9999999' src={images} />
+                        </div>
+                    )
+                } else {
+                    return ("");
+                }
+            }
+        };
         return (
-            <div>
-            <div className="gallery-grid" data-total-slides={images.length}>{ displayImages(images) }</div>
+            <div className="gallery-grid">
+                {displayImages(images)}
             </div>
-        );
-
+        )
     },
-} );
+});
---
+++ src/blocks/video/edit.js
@@ -0,0 +1,113 @@
+/**
+ * BLOCK: AMP Advanced Btn
+ *
+ * Editor for Advanced Btn
+ */
+const { __ } = wp.i18n;
+const { MediaUpload } = wp.editor; //Import MediaUpload from wp.editor
+const {
+	InspectorControls,
+} = wp.blockEditor;
+const {
+	Component,
+	Fragment,
+} = wp.element;
+const {
+	PanelBody,
+	Button,
+	SelectControl,
+	TextControl,
+} = wp.components;
+class ampAdvancedHeading extends Component {
+	constructor() {
+		super(...arguments);
+		this.state = {
+			hideShowUploadButton: arguments[0].attributes.images ? false : true,
+		};
+		// console.log(arguments[0].attributes.images);
+	}
+	GetParamByName(name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	render() {
+		const { attributes, setAttributes } = this.props;
+		const { images, videoSource } = attributes;
+		const getId = (url) => {
+			var a = this.GetParamByName('v', url);
+			return '//www.youtube.com/embed/' + a;
+		}
+		let displayImages = (images) => {
+			if (videoSource) {
+				return (
+					<div className="gallery-item-container">
+						<iframe width="900px" height="500px" className='gallery-item' src={getId(images)} />
+					</div>
+				)
+			} else {
+				//Loops throug the images
+				if (typeof images !== 'undefined') {
+					return (
+						<div className="gallery-item-container">
+							<iframe width="900px" height="500px" className='gallery-item' src={images} />
+						</div>
+					)
+				} else {
+					return ("");
+				}
+			}
+		};
+		return (
+			<Fragment>
+				<div className="gallery-grid">
+					{displayImages(images)}
+				</div>
+				<InspectorControls> {/* For left panel controls */}
+					<PanelBody title={__('Video URL')}>
+						<SelectControl
+							label={__('Link Target', 'amp-blocks')}
+							//value={ btns[ indexcountamp ].target }
+							options={[
+								{ value: 'false', label: __('Internal', 'amp-blocks') },
+								{ value: 'true', label: __('External', 'amp-blocks') },
+							]}
+							onChange={value => {
+								setAttributes({
+									videoSource: value,
+								});
+							}}
+						/>
+						{videoSource && (
+							<TextControl
+								label={__('Please Enter URL', 'amp-blocks')}
+								onChange={(value) => {
+									setAttributes({ images: value });
+									this.setState({ hideShowUploadButton: false })
+								}}
+							/>
+						)}
+					</PanelBody>
+				</InspectorControls>
+				{/* content to display on block slected START */}
+				{this.state.hideShowUploadButton && (
+					<MediaUpload
+						onSelect={(media) => { setAttributes({ images: media.url }); this.setState({ hideShowUploadButton: false }) }}
+						value={images}
+						render={({ open }) => (
+							<Button className="select-images-button is-button is-default is-large" onClick={open}>
+								Upload Video
+				</Button>
+						)}
+					/>
+				)}
+				{/* content to display on block slected END */}
+			</Fragment>
+		);
+	}
+}
+export default (ampAdvancedHeading);
