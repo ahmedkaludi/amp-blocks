d2c799756cb73e4df3f3eac0320078cc48dbb020
Lokesh <jasthilokesh@gmail.com>
2020-01-22 17:42:03 +0530

    removed PLUS icons

--- dist/class-amp-blocks-frontend.php
+++ dist/class-amp-blocks-frontend.php
@@ -3389,7 +3389,7 @@ class Amp_Blocks_Frontend
 		}
 		// Lets register all the block styles.
 		wp_register_style( 'amp-blocks-btn', AMP_BLOCKS_DIR_URI . 'dist/blocks/btn.style.build.css', array(), AMP_BLOCKS_VERSION );
-		//wp_register_style('amp-blocks-rowlayout', AMP_BLOCKS_DIR_URI . 'dist/blocks/row.style.build.css', array(), AMP_BLOCKS_VERSION);
+		wp_register_style('amp-blocks-rowlayout', AMP_BLOCKS_DIR_URI . 'dist/blocks/row.style.build.css', array(), AMP_BLOCKS_VERSION);
 		wp_register_style('amp-blocks-rowlayout', AMP_BLOCKS_DIR_URI . 'dist/blocks/image.style.build.css', array(), AMP_BLOCKS_VERSION);
 	}
 
