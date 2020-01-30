fe40dc58cf08a226025645e935e6976a9af6174e
Lokesh <jasthilokesh@gmail.com>
2020-01-30 19:06:59 +0530

    implemented Canvas Mode

--- src/blocks.js
+++ src/blocks.js
@@ -27,32 +27,89 @@ window.kb = {
 
 //wp.i18n.setLocaleData( { '': {} }, 'amp-blocks' );
 if (typeof amp_blocks_default_size !== 'undefined') {
-	wp.data.dispatch( 'core/editor' ).updateEditorSettings( { maxWidth: amp_blocks_default_size } );
+	wp.data.dispatch('core/editor').updateEditorSettings({ maxWidth: amp_blocks_default_size });
 }
-document.addEventListener( 'DOMContentLoaded', appendImportButton );
+document.addEventListener('DOMContentLoaded', appendImportButton);
 
 function appendImportButton() {
-	let node = document.querySelector( '.edit-post-header-toolbar' );
-	let newElem = document.createElement( 'div' );
+	let node = document.querySelector('.edit-post-header-toolbar');
+	let newElem = document.createElement('div');
 	let html = '<div class="amp-block-defaults-modal">';
-	html += `<button class="button button-primary" id="AMPBlocksImportLayoutBtn" > AMP Blocks Prebuilt Library</button>`;
+	html += `<button class="button button-primary" id="ampblockCanvasbutton">Enter Canvas Mode</button><button class="button button-primary" id="AMPBlocksImportLayoutBtn" > AMP Blocks Prebuilt Library</button>`;
 	html += '</div>';
 	newElem.innerHTML = html;
-	node.appendChild( newElem );
-	document.getElementById( 'AMPBlocksImportLayoutBtn' ).addEventListener( 'click', ampBlockImportPrebuiltLibrary );
+	node.appendChild(newElem);
+	document.getElementById('ampblockCanvasbutton').addEventListener('click', canvasbutton);
+
+	document.getElementById('AMPBlocksImportLayoutBtn').addEventListener('click', ampBlockImportPrebuiltLibrary);
+	jQuery('[aria-label="Settings"]').click(function () {
+		setTimeout(function () {
+			addListeners();
+		}, 1000);
+	});
+
 }
 
+
+function canvasbutton() {
+
+	var isFullScreenMode = wp.data.select('core/edit-post').isFeatureActive('fullscreenMode');
+	if (!isFullScreenMode) {
+		if (!jQuery('.components-icon-button').hasClass('is-toggled')) {
+			jQuery('.components-icon-button').trigger('click');
+		}
+		document.getElementsByClassName('edit-post-sidebar')[0].style.top = '20%';
+		document.getElementsByClassName('edit-post-sidebar')[0].style.right = '5%';
+		document.getElementsByClassName('edit-post-layout__content')[0].style.margin = '0px';
+		document.getElementById("ampblockCanvasbutton").innerText = 'Exit Canvas Mode';
+	} else {
+		document.getElementsByClassName('edit-post-sidebar')[0].style.top = null;
+		document.getElementsByClassName('edit-post-sidebar')[0].style.right = null;
+		document.getElementsByClassName('edit-post-sidebar')[0].style.left = null;
+		document.getElementsByClassName('edit-post-sidebar')[0].style.position = null;
+		document.getElementsByClassName('edit-post-layout__content')[0].style.margin = null;
+		document.getElementById("ampblockCanvasbutton").innerText = 'Enter Canvas Mode';
+	}
+	wp.data.dispatch('core/edit-post').toggleFeature('fullscreenMode');
+	setTimeout(function () {
+		addListeners();
+	}, 1000);
+}
 var node;
 
-function ampBlockImportPrebuiltLibrary( e ) {
+function ampBlockImportPrebuiltLibrary(e) {
 	e.preventDefault();
-	if (! node) {
-		node = document.createElement( 'div' );
-		node.setAttribute( 'id', 'prebuilt_modal_top_button' );
-		document.body.appendChild( node );
+	if (!node) {
+		node = document.createElement('div');
+		node.setAttribute('id', 'prebuilt_modal_top_button');
+		document.body.appendChild(node);
 	}
-	wp.element.render( <Prebuilt_Modal modalOpen={ true }/>, node );
+	wp.element.render(<Prebuilt_Modal modalOpen={true} />, node);
 
+}
+
+function addListeners() {
+	var el = jQuery('.components-panel__header.edit-post-sidebar-header.edit-post-sidebar__panel-tabs')[0];
+	if (el) {
+		el.addEventListener('mousedown', mouseDown, false);
+	}
+	window.addEventListener('mouseup', mouseUp, false);
+
+}
+
+function mouseUp() {
+	window.removeEventListener('mousemove', moveEditPostSidebar, true);
+}
+
+function mouseDown(e) {
+	window.addEventListener('mousemove', moveEditPostSidebar, true);
+}
+
+function moveEditPostSidebar(e) {
+	var editPostSidebar = document.getElementsByClassName('edit-post-sidebar')[0];
+	editPostSidebar.style.position = 'absolute';
+	editPostSidebar.style.top = e.clientY + 'px';
+	editPostSidebar.style.left = e.clientX + 'px';
 }
 
 
