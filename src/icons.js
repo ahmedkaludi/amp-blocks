d7de5f27c0e7139c03bf7b6eecca4cebb99fe484
Lokesh <jasthilokesh@gmail.com>
2020-02-08 13:27:21 +0530

    changed block icon

--- src/icons.js
+++ src/icons.js
@@ -1,22 +1,6 @@
 const icons = {};
 icons.blockRow =
-	<svg width="20px" height="20px" viewBox='0 0 48 48' xmlns='http://www.w3.org/2000/svg' fillRule='evenodd'
-		 clipRule='evenodd' strokeLinejoin='round' strokeMiterlimit='1.414'>
-		<path
-			d='M46.506,7.877l-45.012,0c-0.484,0 -0.882,0.398 -0.882,0.882l0,23.645c0,0.484 0.398,0.882 0.882,0.882l45.012,0c0.484,0 0.882,-0.398 0.882,-0.882l0,-23.65c-0.003,-0.482 -0.4,-0.877 -0.882,-0.877Zm-0.882,23.65l-43.248,0l0,-21.886l43.248,0l0,21.886Z'
-			fill='#179cd4' fillRule='nonzero'/>
-		<rect x='4.587' y='11.533' width='38.825' height='18.096' fill='#179cd4'
-		/>
-		<path
-			d='M43.413,29.63l-38.826,0l0,-18.097l38.826,0l0,18.097Zm-37.061,-16.332l0,14.567l35.296,0l0,-14.567l-35.296,0Z'
-			fill='#179cd4'/>
-		<path
-			d='M24,40.123c4.365,0 7.957,-3.592 7.957,-7.957c0,-4.365 -3.592,-7.957 -7.957,-7.957c-4.365,0 -7.957,3.592 -7.957,7.957c0,0.001 0,0.002 0,0.003c0,4.363 3.591,7.954 7.954,7.954c0.001,0 0.002,0 0.003,0Z'
-			fill='#179cd4' fillRule='nonzero' stroke='#fff' strokeWidth='1.76'/>
-		<path
-			d='M20.811,31.539l2.307,0l0,-2.309c0,-0.484 0.398,-0.882 0.882,-0.882c0.484,0 0.882,0.398 0.882,0.882l0,2.309l2.307,0c0.483,0 0.882,0.398 0.882,0.882c0,0.484 -0.399,0.882 -0.882,0.882l-2.307,0l0,2.307c0,0.484 -0.398,0.882 -0.882,0.882c-0.484,0 -0.882,-0.398 -0.882,-0.882l0,-2.307l-2.307,0c-0.483,0 -0.882,-0.398 -0.882,-0.882c0,-0.484 0.399,-0.882 0.882,-0.882Z'
-			fill='#fff' fillRule='nonzero'/>
-	</svg>;
+<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false"><path fill="none" d="M0 0h24v24H0V0z"></path><g><path d="M4,4H20a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4ZM4 6V18H8V6Zm6 0V18h4V6Zm6 0V18h4V6Z"></path></g></svg>;
 icons.row = <svg viewBox="0 0 60 30" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd"
 				 clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414">
 	<rect x="0.000" y="0.000" width="60.000" height="30.000" fill="#d5dadf"
